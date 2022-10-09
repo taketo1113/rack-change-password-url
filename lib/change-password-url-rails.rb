ede4e57c00457fb5dc56c13f51b73d342570c673
Taketo Takashima <t.taketo1113@gmail.com>
2022-10-09 21:54:06 +0900

    Init commit

---
+++ lib/change-password-url-rails.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "change-password-url-rails/version"
+
+module ChangePasswordUrlRails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/change-password-url-rails/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ChangePasswordUrlRails
+  VERSION = "0.1.0"
+end
