a9b137996f5ae6b6a9ed4eb5c1f921744082122f
Taketo Takashima <t.taketo1113@gmail.com>
2022-10-11 23:03:11 +0900

    Added settings for rails

--- lib/change-password-url-rails.rb
+++ lib/change-password-url-rails.rb
@@ -3,6 +3,8 @@
 require_relative "change-password-url-rails/middleware"
 require_relative "change-password-url-rails/version"
 
+require 'change-password-url-rails/railtie' if defined?(::Rails)
+
 module ChangePasswordUrlRails
   class Error < StandardError; end
 end
---
+++ lib/change-password-url-rails/rails_options.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module ChangePasswordUrlRails
+  class RailsOptions
+    attr_accessor :redirect_path
+  end
+end
